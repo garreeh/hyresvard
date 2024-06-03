8ac5ef96a2ebad2fd3025be519f82ea7bda905bb
Garry Gajultos <gajultos.garry123@gmail.com>
2024-06-03 17:42:58 +0800

    first commit

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this template!
+
+Template Name: Hyresvard
+Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
---
+++ includes/navigation.php
@@ -0,0 +1,54 @@
+<!-- ======= Header/Navbar ======= -->
+<nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+  <div class="container">
+    <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+    <a class="navbar-brand text-brand color-b" href="index.php">HYRESVARD</a>
+
+    <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link active" href="index.php">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="about.php">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="property-grid.php">Property</a>
+        </li>
+
+        <!-- <li class="nav-item">
+          <a class="nav-link " href="blog-grid.php">Blog</a>
+        </li> -->
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Property Dimensions</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item " href="property-single.php">Property Single</a>
+            <a class="dropdown-item " href="blog-single.php">Property Duplex</a>
+          </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="agents-grid.php">Agents</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="contact.php">Contact</a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- <button type="button" class="btn btn-b-n navbar-toggle-box navbar-toggle-box-collapse" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
+      <i class="bi bi-search"></i>
+    </button> -->
+
+  </div>
+</nav>
+<!-- End Header/Navbar -->
